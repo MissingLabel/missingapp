33475bed60374c1c8bfe7e44d8e089cb914e6647
Shreya Patel <shreya317@gmail.com>
2014-12-02 11:05:09 -0600

    added alerts for testing

--- www/js/swipe.js
+++ www/js/swipe.js
@@ -3,6 +3,8 @@ $(document).on('deviceready', function(){
   var swipeEvents = new Hammer(myElement);
   var pagePosition = 1;
   swipeEvents.on('swipeleft', function(ev) {
+    alert(viewTemplating);
+    alert(currentProductData);
     if(pagePosition === 1) {
       $("#left-button").css("display", "none");
       viewTemplating.showLocationTemplate(currentProductData);
@@ -12,6 +14,8 @@ $(document).on('deviceready', function(){
     };
     pagePosition -=1;
   }).on('swiperight', function(ev) {
+    alert(viewTemplating);
+    alert(currentProductData);
     if(pagePosition === 1){
       $("#right-button").css("display", "none");
       viewTemplating.showProduceProfile(currentProductData);
